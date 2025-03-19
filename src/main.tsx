b75fc5415c875473b4e6732e109b5074ed7cd8bb
Neda <neda@Nedas-MacBook-Pro.local>
2025-03-19 05:39:07 -0700

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,23 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import App from './App'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // React 19 uses createRoot
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <QueryClientProvider client={new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5, // 5 minutes
+            gcTime: 1000 * 60 * 60, // 1 hour
+            retry: 1,
+          },
+        },
+      })}>
+        <App />
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>
 ) 
